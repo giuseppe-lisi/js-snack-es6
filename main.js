4b1c7fc69000cc361687b382e888aa605c565d8f
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2025-11-14 13:47:34 +0100

    utilizzati due metodi diversi per trovare la bici con il peso minore

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// definisco un array di oggetti "bici" dove ogni oggetto 
+// ha come proprietà un nome e un peso
 const bikes = [
 
     {nome: "Trek Madone", peso: 6.4},
@@ -13,12 +15,37 @@ const bikes = [
 
 ]
 
-let lowestWeight = 100;
+// variabili di appoggio per il peso della bici e il suo nome
+let lowestWeight = bikes[0].peso;
+let lowestWeightBike;
+// ad ogni iterazione controlla se il peso della bici 
+// è inferiore al peso della bici precedente. Se è inferiore
+// aggiorna il peso più basso e salva l'indice
+// della bici con il peso minore trovato fino ad ora.
 for (let i = 1; i < bikes.length; i++) {
     if (bikes[i].peso < lowestWeight) {
         lowestWeight = bikes[i].peso;
+        lowestWeightBike = bikes[i];
     }
-    console.log(lowestWeight);
-    
 }
+// stampa a schermo la bici di peso più basso trovata
+document.getElementById("lowestWeight").innerHTML = `<div>Bici con peso minore con for loop: <span style="color: green;">${lowestWeightBike.nome}</span></div>`;
+
+// con il metodo sort() - o toSorted() in questo caso
+// per non modificare l'array originale - possiamo fornire
+// una comparatorFn che restituisce 1, -1, 0 in base al valore
+// che specifichiamo per return
+let sortedBikes = bikes.toSorted(
+    // passo alla arrow function i due oggetti dell'array
+    // che verranno comparati
+    (a, b) => {
+    // se peso1 - peso2 = positiveNum restituisco 1 —> a > b
+    // se peso1 - peso2 = negativeNum restituisco -1 —> a < b
+    // se peso1 - peso2 = 0 restituisco 0 —> a = b
+    return a.peso-b.peso;
+});
+
+// a questo punto mi troverò all'indice 0 dell'array di bici
+// la bici con il peso minore
+document.getElementById("lowestWeight").innerHTML += `<div>Bici con peso minore con sort() method: <span style="color: red;">${sortedBikes[0].nome}</span></div>`;
 
